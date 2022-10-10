54f2c5a2590004cca11bea56869bee214494bf3a
vtfeitosa <vfmidia@hotmail.com>
2022-10-10 01:21:21 -0300

    fix: correction of comments inline at .js and .html files

--- public/javascript/item.js
+++ public/javascript/item.js
@@ -1,6 +1,8 @@
 const selector = document.getElementById("selector");
 const SelectedFunkoImg = document.querySelector(".SelectedFunkoImg");
 
+// Função dedicada a captar o objeto com as informações sobre o produto, através do select
+// Falta integrar API para captar objeto
 
 function get_img(){
     const option = selector.value;
