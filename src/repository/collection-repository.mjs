71dd761814afe245a96f9a3a1492e57f743d85cf
Diego Oliveira <diegoc.oliveira@gmail.com>
2022-10-11 11:17:13 -0300

    resolvendo conflitos

--- src/repository/collection-repository.mjs
+++ src/repository/collection-repository.mjs
@@ -154,11 +154,9 @@ export default class CollectionRepository {
         let error = null;
         try {
             const query = `SELECT collection.id, funko.id as funko_id, funko.name as funko_name, "user".name as user_name FROM starcollection.collection 
-            INNER JOIN starcollection.funko ON funko.id = collection.funko_id
-                  AND funko.deleted_at IS NULL
-            INNER JOIN starcollection."user" ON "user".id = collection.user_id
-                  AND "user".deleted_at IS NULL
-            WHERE collection.is_exchange = true AND collection.deleted_at is null`;
+            RIGHT JOIN starcollection.funko ON funko.id = collection.funko_id
+            RIGHT JOIN starcollection."user" ON "user".id = collection.user_id
+            WHERE collection.is_exchange = true AND collection.deleted_at is null AND "user".deleted_at is null`;
             const result = await pool.query(query);
             for (let i = 0; i < result.rows.length; i++) {
                 const row = result.rows[i];
